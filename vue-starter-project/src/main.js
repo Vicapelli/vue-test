508222227467051e7f944be6c1ffda0b3808fd38
Victor Capelli <victorbustocapelli@hotmail.com>
2021-11-29 11:29:49 -0300

    Add bootstrap and router

--- vue-starter-project/src/main.js
+++ vue-starter-project/src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+
 
 new Vue({
   router,
